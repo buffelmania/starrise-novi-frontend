760a7581e811a9b9dd4eadd70f1200de60df46b8
buffelmania <74376505+buffelmania@users.noreply.github.com>
2023-01-23 12:36:08 +0100

    Eerste setup voor api gemaakt

--- src/Pages/ZonEnMaan.js
+++ src/Pages/ZonEnMaan.js
@@ -1,15 +1,54 @@
 import React from "react";
+import {useState} from "react";
 import axios from 'axios';
 import '../components/ZonEnMaan/ZonEnMaanStyles.css'
+import {useEffect} from "react";
 
 function ZonEnMaan(){
+const [data,setData] = useState({})
+const [location, setLocation] = useState('')
+
+    const url ='https://api.ipgeolocation.io/astronomy?apiKey=84ecef0b73d34b7c896e3e4dd9592438&location=${location}'
 
 
+   const searchLocation = useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`https://api.ipgeolocation.io/astronomy?apiKey=84ecef0b73d34b7c896e3e4dd9592438&location=${location}`);
+                console.log(response.data);
+                setData(response.data);
+                console.log(response.data)
+            } catch (e) {
+                console.error(e);
+            }
+        }
 
-    const url ='https://api.ipgeolocation.io/astronomy?apiKey=84ecef0b73d34b7c896e3e4dd9592438&location=New%20York,%20US'
+        if (location) {
+            fetchData();
+        }
+    }, [location]);
+
+    /*const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data)
+            })
+            setLocation('')
+        }
+    }*/
+
     return(
         <div className="ZonEnMaan">
             <div className="container">
+                <div className="search">
+                    <input
+                        value={location}
+                        onChange={event => setLocation(event.target.value)}
+                        onKeyUp={searchLocation}
+                        placeholder='Enter Location'
+                        type="text"/>
+                </div>
                 <div className="top">
                     <div className="location">
                         <p>Leiden</p>
@@ -23,6 +62,10 @@ function ZonEnMaan(){
 
                 </div>
                 <div className="bottom">
+                    <div className="DistanceToSun">
+                        <p className='bold'>100000</p>
+                    <p>Afstand tot de zon</p>
+                    </div>
 
                 </div>
             </div>
