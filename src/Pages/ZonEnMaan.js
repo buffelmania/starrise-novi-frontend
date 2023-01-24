5a90e4ff11c611e4c55ba70b29552682d925513a
buffelmania <74376505+buffelmania@users.noreply.github.com>
2023-01-24 15:40:02 +0100

    Api werkt, css moet nog aangepast worden.

--- src/Pages/ZonEnMaan.js
+++ src/Pages/ZonEnMaan.js
@@ -3,61 +3,66 @@ import {useState} from "react";
 import axios from 'axios';
 import '../components/ZonEnMaan/ZonEnMaanStyles.css'
 import {useEffect} from "react";
+import '../components/ZonEnMaan/searchBar/SearchBar';
+import '../components/ZonEnMaan/tabBarMenu/TabBarMenu';
+import SearchBar from "../components/ZonEnMaan/searchBar/SearchBar";
+import TabBarMenu from "../components/ZonEnMaan/tabBarMenu/TabBarMenu";
 
 function ZonEnMaan(){
 const [data,setData] = useState({})
 const [location, setLocation] = useState('')
 
-    const url ='https://api.ipgeolocation.io/astronomy?apiKey=84ecef0b73d34b7c896e3e4dd9592438&location=${location}'
+    const url ='https://api.ipgeolocation.io/astronomy?apiKey=d793088378b847f6b524c3091aa6ecfd&location=${location}'
 
 
-   const searchLocation = useEffect(() => {
+
+    useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.get(`https://api.ipgeolocation.io/astronomy?apiKey=84ecef0b73d34b7c896e3e4dd9592438&location=${location}`);
+                const response = await axios.get(`https://api.ipgeolocation.io/astronomy?apiKey=d793088378b847f6b524c3091aa6ecfd&location=${location}`);
                 console.log(response.data);
                 setData(response.data);
-                console.log(response.data)
             } catch (e) {
                 console.error(e);
             }
         }
 
-        if (location) {
+       if (location) {
             fetchData();
         }
     }, [location]);
 
-    /*const searchLocation = (event) => {
-        if (event.key === 'Enter') {
-            axios.get(url).then((response) => {
-                setData(response.data)
-                console.log(response.data)
-            })
-            setLocation('')
-        }
-    }*/
-
     return(
         <div className="ZonEnMaan">
             <div className="container">
-                <div className="search">
-                    <input
-                        value={location}
-                        onChange={event => setLocation(event.target.value)}
-                        onKeyUp={searchLocation}
-                        placeholder='Enter Location'
-                        type="text"/>
-                </div>
-                <div className="top">
-                    <div className="location">
-                        <p>Leiden</p>
+                <div className="weather-container">
+
+                    {/*HEADER -------------------- */}
+                    <div className="weather-header">
+                        <SearchBar setLocationHandler={setLocation}/>
+
+                        <span className="location-details">
+            {Object.keys(data).length > 0 &&
+                <>
+
+                    <h3>{data.name}</h3>
+                    <h2>Zonsopkomst: {data.sunrise}</h2>
+                    <h2>Zonsondergang: {data.sunset}</h2>
+                   <h2>Afstand tot de zon: {data.sun_distance}</h2>
+
+                </>
+            }
+
+          </span>
                     </div>
-                    <div className="sunrise">
-                        <h1>07:00</h1>
-                    </div>
-                    <div className="sunset">
-                        <h1>16:00</h1>
+
+                    {/*CONTENT ------------------ */}
+                    <div className="weather-content">
+                        <TabBarMenu/>
+
+                        <div className="tab-wrapper">
+                            Alle inhoud van de tabbladen komt hier!
+                        </div>
                     </div>
 
                 </div>
