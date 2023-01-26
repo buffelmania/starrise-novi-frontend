1c2dbf76ca0702f22f1a96448a886f1c881f875d
buffelmania <74376505+buffelmania@users.noreply.github.com>
2023-01-26 14:37:55 +0100

    Gebruiker kan nu inloggen

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom";
 import "../components/account/AccountStyles.css"
 import {UserAuth} from "../context/AuthContext";
 
 function Account(){
     const {user, logout} = UserAuth();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+try {
+    await logout()
+    navigate('/')
+    } catch (e){
+    console.log(e.message)
+}
+    }
     return (
         <div className="Account">
             <h2>Account</h2>
             <p>Gebruiker Email: {user && user.email }</p>
-            <button>Log uit</button>
+            <button onClick={handleLogout}>Log uit</button>
         </div>
     )
 }
--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "../Signin/SignInStyles.css"
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {UserAuth} from "../context/AuthContext";
 
 
+const Signin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const { signIn } = UserAuth();
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('')
+        try {
+            await signIn(email, password)
+            navigate('/account')
+        } catch (e) {
+            setError(e.message)
+            console.log(e.message)
+        }
+    };
 
-
-
-
-function Signin() {
     return (
         <div>
             <div className="Signin">
@@ -17,22 +31,20 @@ function Signin() {
 <h2>Login om te zien wanneer de zon en maan opkomen en weer ondergaan.</h2>
             <h2>Heb je nog geen account? Maak dan snel een <Link to="/SignUp">account aan.</Link></h2>
 </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>
                         Email adres
                     </label>
-                    <input type="email"/>
+                    <input onChange={(e) =>setEmail(e.target.value)} type="email"/>
                 </div>
-            </form>
-            <form>
                 <div>
                     <label>
                         Wachtwoord
                     </label>
-                    <input type="password"/>
+                    <input onChange={(e) =>setPassword(e.target.value)} type="password"/>
                 </div>
-                <button>Maak account aan</button>
+                <button>Login</button>
             </form>
 
 </div>
